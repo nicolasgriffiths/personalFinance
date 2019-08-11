5552295c204e4a1f3a8c7e78ebd319616dc7f838
Nicolas Griffithsclear <nicool80@hotmail.com>
2019-08-11 17:27:02 +0100

    fixed warnings and improved string formatting

--- plots.py
+++ plots.py
@@ -20,9 +20,9 @@ def plot_total_savings(ax, x_data, y_data, currency):
     ax.plot_date(x_data, get_poly_fit(x_data, y_data, 1), '--b')
     ax.plot_date(x_data, get_poly_fit(x_data, y_data, 2), '--r')
 
-    text_latest = ''.join(
-        r'Total savings: %.2f' % (y_data.iloc[-1]))
-    ax.text(0.05, 0.95, text_latest, transform=ax.transAxes,
+    text_latest = 'Total savings: {:.2f} {cur}'.format(
+        y_data.iloc[-1], cur=currency)
+    ax.text(0.02, 0.95, text_latest, transform=ax.transAxes,
             fontsize=7, verticalalignment='top',
             bbox={'boxstyle': 'round', 'facecolor': 'wheat', 'alpha': 0.5})
 
@@ -37,18 +37,24 @@ def plot_incremental_savings(ax, x_data, y_data, currency):
     ax.plot_date(x_data, get_poly_fit(x_data, y_data, 1), '--r')
     ax.plot_date(x_data, get_poly_fit(x_data, y_data, 0), '--b')
 
-    text_stats = '\n'.join((
-        r'$\mu=%.2f$' % (y_data.mean()),
-        r'$\mathrm{median}=%.2f$' % (y_data.median()),
-        r'$\sigma=%.2f$' % np.sqrt(y_data.var())))
-    ax.text(0.05, 0.95, text_stats, transform=ax.transAxes,
+    text_stats = ('μ={:.2f} {cur}\n'
+                  'median={:.2f} {cur}\n'
+                  'σ={:.2f} {cur}'
+                  .format(y_data.mean(),
+                          y_data.median(),
+                          np.sqrt(y_data.var()),
+                          cur=currency))
+    ax.text(0.02, 0.95, text_stats, transform=ax.transAxes,
             fontsize=7, verticalalignment='top',
             bbox={'boxstyle': 'round', 'facecolor': 'wheat', 'alpha': 0.5})
 
-    text_latest = '\n'.join((
-        r'Saved this month %.2f' % (y_data.iloc[-1]),
-        r'%.2f wrt last month' % (y_data.iloc[-1] - y_data.iloc[-2]),
-        r'%.2f wrt mean' % (y_data.iloc[-1] - y_data.mean())))
+    text_latest = ('Month savings: {:.2f} {cur}\n'
+                   '{:.2f} wrt last month\n'
+                   '{:.2f} wrt mean'
+                   .format(y_data.iloc[-1],
+                           y_data.iloc[-1] - y_data.iloc[-2],
+                           y_data.iloc[-1] - y_data.mean(),
+                           cur=currency))
     ax.text(0.25, 0.95, text_latest, transform=ax.transAxes,
             fontsize=7, verticalalignment='top',
             bbox={'boxstyle': 'round', 'facecolor': 'wheat', 'alpha': 0.5})
@@ -61,7 +67,7 @@ def plot_incremental_savings(ax, x_data, y_data, currency):
 def plot_data(finance_data, notes, currency):
     dates = matplotlib.dates.date2num(finance_data.index)
     # TODO Add notes to plots
-    fig, (ax1, ax2) = plt.subplots(2, 1)
+    _fig, (ax1, ax2) = plt.subplots(2, 1)
     plot_total_savings(
         ax1, dates, finance_data['Total Savings'], currency)
     plot_incremental_savings(
