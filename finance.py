896eb8f09e619a033a201ae720b845af17831081
Nicolas Griffiths <nicool80@hotmail.com>
2021-02-24 20:13:48 +0100

    include pension as an option

--- finance.py
+++ finance.py
@@ -5,11 +5,22 @@ from currency import adjust_currency, CURRENCY_STR
 from plots import plot_data
 
 
-def clean_data(finance_data_raw):
+def clean_data(finance_data_raw, include_pension):
     '''Extract notes and currencies from the raw financial data'''
     notes = finance_data_raw[[NOTES_STR]].drop(CURRENCY_STR, 0)
     currency_symbols = finance_data_raw.loc[[CURRENCY_STR]].drop(NOTES_STR, 1)
     finance_data = finance_data_raw.drop(CURRENCY_STR, 0).drop(NOTES_STR, 1)
+
+    if not include_pension:
+        cols_to_drop = []
+        for col_name in finance_data.columns:
+            if "Pension" in col_name:
+                cols_to_drop.append(col_name)
+        
+        for col_name in cols_to_drop:
+            currency_symbols = currency_symbols.drop(col_name, 1)
+            finance_data = finance_data.drop(col_name, 1)
+
     return notes, currency_symbols, finance_data
 
 
@@ -33,9 +44,8 @@ def run(args):
     # Load data
     finance_data_raw = pd.read_excel(args.data_path, index_col=0)
 
-    notes, currency_symbols, finance_data = clean_data(finance_data_raw)
-    finance_data = adjust_currency(
-        args.currency, finance_data, currency_symbols)
+    notes, currency_symbols, finance_data = clean_data(finance_data_raw, args.include_pension)
+    finance_data = adjust_currency(args.currency, finance_data, currency_symbols)
     finance_data = compute_savings(finance_data)
     plot_data(finance_data, notes, args.currency)
 
@@ -46,5 +56,7 @@ if __name__ == '__main__':
                         help='Target currency in which the results will be displayed')
     parser.add_argument('--data-path', type=str, default='data/data.xlsx',
                         help='Path to formatted xlsx file')
+    parser.add_argument('--include-pension', action='store_true', default=False,
+                        help='Wether to include columns containing \"Pension\" in the calculations')
     args = parser.parse_args()
     run(args)
