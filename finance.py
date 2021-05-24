8fd7a0e3f83edcf82083d39c75135cdd6f885fe0
Nicolas Griffiths <nicool80@hotmail.com>
2021-05-24 20:04:42 +0100

    format with black

--- finance.py
+++ finance.py
@@ -6,15 +6,14 @@ from plots import plot_data
 
 
 def clean_data(finance_data_raw, include_pension, include_stock):
-    '''Extract notes and currencies from the raw financial data'''
+    """Extract notes and currencies from the raw financial data"""
     if not include_pension:
         cols = [c for c in finance_data_raw.columns if "pension" not in c.lower()]
         finance_data_raw = finance_data_raw[cols]
-    
+
     if not include_stock:
         cols = [c for c in finance_data_raw.columns if "stock" not in c.lower()]
         finance_data_raw = finance_data_raw[cols]
-
 
     notes = finance_data_raw[[NOTES_STR]].drop(CURRENCY_STR, 0)
     currency_symbols = finance_data_raw.loc[[CURRENCY_STR]].drop(NOTES_STR, 1)
@@ -24,10 +23,10 @@ def clean_data(finance_data_raw, include_pension, include_stock):
 
 
 def compute_savings(finance_data):
-    '''Compute total and incremental savings based on clean financial data'''
-    finance_data['Total Savings'] = finance_data.sum(axis=1)
-    finance_data['Incremental Savings'] = finance_data['Total Savings'].diff()
-    finance_data.iloc[0, finance_data.columns.get_loc('Incremental Savings')] = 0
+    """Compute total and incremental savings based on clean financial data"""
+    finance_data["Total Savings"] = finance_data.sum(axis=1)
+    finance_data["Incremental Savings"] = finance_data["Total Savings"].diff()
+    finance_data.iloc[0, finance_data.columns.get_loc("Incremental Savings")] = 0
 
     return finance_data
 
@@ -35,20 +34,42 @@ def compute_savings(finance_data):
 def run(args):
     finance_data_raw = pd.read_excel(args.data_path, index_col=0)
     notes, currency_symbols, finance_data = clean_data(finance_data_raw, args.include_pension, args.include_stock)
-    finance_data = adjust_currency(args.currency, finance_data, currency_symbols)
+    finance_data = adjust_currency(args.currency, finance_data, currency_symbols, args.force_manual_exchange)
     finance_data = compute_savings(finance_data)
     plot_data(finance_data, notes, args.currency)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--currency', type=str, default='EUR',
-                        help='Target currency in which the results will be displayed')
-    parser.add_argument('--data-path', type=str, default='data/data.xlsx',
-                        help='Path to formatted xlsx file')
-    parser.add_argument('--include-pension', action='store_true', default=False,
-                        help='Wether to include columns containing \"Pension\" in the calculations')
-    parser.add_argument('--include-stock', action='store_true', default=False,
-                        help='Wether to include columns containing \"Stock\" in the calculations')
+    parser.add_argument(
+        "--currency",
+        type=str,
+        default="EUR",
+        help="Target currency in which the results will be displayed",
+    )
+    parser.add_argument(
+        "--data-path",
+        type=str,
+        default="data/data.xlsx",
+        help="Path to formatted xlsx file",
+    )
+    parser.add_argument(
+        "--include-pension",
+        action="store_true",
+        default=False,
+        help='Wether to include columns containing "Pension" in the calculations',
+    )
+    parser.add_argument(
+        "--include-stock",
+        action="store_true",
+        default=False,
+        help='Wether to include columns containing "Stock" in the calculations',
+    )
+    parser.add_argument(
+        "--force-manual-exchange",
+        action="store_true",
+        default=False,
+        help="Wether to force to not use automatic currency conversions",
+    )
     args = parser.parse_args()
     run(args)
